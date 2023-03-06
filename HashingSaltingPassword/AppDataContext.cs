b1545eb0050a3677ea496684217c4f17c3b29b55
Smarter Software <smartersoftware.itservices@gmail.com>
2023-03-06 23:50:57 +0530

    Add project files.

---
+++ HashingSaltingPassword/AppDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDataContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=HashingApp;Integrated Security=True;");
+    }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ HashingSaltingPassword/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Salt { get; set; }
+}
